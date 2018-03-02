9208b14eb109f361acaf7bab458a52fd0217c1a3
zecollokaris <collo.kariss@gmail.com>
2018-03-02 12:42:10 +0300

    Changes to ping pong to make it work

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
+//Business logic
+function pingPong(number) {
+  var myArray = [];
+  for(var a=0; a<=number; a++) {
+    if (a % 15=== 0){
+      myArray.push("Ping Pong!");
+    } else if (a % 5=== 0) {
+      myArray.push("Pong!");
+    } else if(a % 3=== 0) {
+      myArray.push("Ping!");
+    }
+      else {myArray.push (a);}
+    };
+    return myArray;
+  };
+
+//User Interface logic
 $(document).ready(function() {
- //Opening the menu
- $("#open-menu").click(function() {
-   $("aside").animate({"left": 0}, 2000);
- });
- //Closing the menu
-$("#close-menu").click(function() {
- $("aside").animate({"left":"-100%"}, 2000);
- });
-});
-//END OF MENU BUTTON
+    $("form#ping-pong").submit(function(event) {
+      event.preventDefault();
+      $(".col-result").empty();
+      var number = parseInt($("input#number").val());
+      var result = pingPong(number)+ '';
+      var num = result.split(",").join("<br>");
+      $("div.result1").addClass("col-result").append("<li class='col-result'"+ num + "</li>");
+    });
+  });
